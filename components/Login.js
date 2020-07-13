b54cf410a8641a1b546e394dda2229ef7de67886
M7CORE <65249806+M7CORE@users.noreply.github.com>
2020-07-13 06:09:08 -0700

    Update local and deployment URL

--- components/Login.js
+++ components/Login.js
@@ -4,47 +4,40 @@ const BASE_URL = 'https://mernstack-1.herokuapp.com/';
 //FOR LOCAL TESTING USE LOCALHOST URL
 //const BASE_URL = 'http://localhost:5000/';
 
-function signup()
+function Login()
 {
     var loginName;
     var loginPassword;
-    var emailName;
 
-   // const [message,setMessage] = useState('');
+    const [message,setMessage] = useState('');
 
-    const doSignup = async event => 
-
-    
+    const doLogin = async event => 
     {
-        
         event.preventDefault();
 
-        var js = '{"email":"'
-                + emailName.value
-                + '","login":"'
+        var js = '{"login":"'
             + loginName.value
             + '","password":"'
             + loginPassword.value +'"}';
 
-            try
+        try
         {    
-            const response = await fetch(BASE_URL + 'api/signup',
+            const response = await fetch(BASE_URL + 'api/login',
                 {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
             var res = JSON.parse(await response.text());
-            
-            if( res.id > 0 )
+
+            if( res.id <= 0 )
             {
-                //setMessage('User already exists');
-                alert("user exists already");
+                setMessage('User/Password combination incorrect');
             }
             else
             {
                 var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
                 localStorage.setItem('user_data', JSON.stringify(user));
 
-               // setMessage('');
-                window.location.href = '/';
+                setMessage('');
+                window.location.href = '/cards';
             }
         }
         catch(e)
@@ -54,21 +47,27 @@ function signup()
         }    
     };
 
+    const doSignup = async event => 
+    {
+        
+                window.location.href = '/signup';
+         
+    };
 
     return(
       <div id="loginDiv">
-        <form onSubmit={doSignup}>
-        <span id="inner-title">PLEASE SIGN UP</span><br />
-        <input type="text" id="emailName" placeholder="email" ref={(c) => emailName = c} /><br />
+        <form onSubmit={doLogin}>
+        <span id="inner-title">PLEASE LOG IN</span><br />
         <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} /><br />
         <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} /><br />
-        <input type="submit" id="signupButton" class="buttons" value = "Sign Up"
+        <input type="submit" id="loginButton" class="buttons" value = "Do It"
+          onClick={doLogin} />
+          <input type="submit" id="loginButton" class="buttons" value = "Signup?"
           onClick={doSignup} />
-
         </form>
-        
+        <span id="loginResult">{message}</span>
      </div>
     );
 };
 
-export default signup;
+export default Login;
