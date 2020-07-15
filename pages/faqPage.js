d66c87f33ff955a93d75b46325a43150f59e335a
maxmc2234 <65358650+maxmc2234@users.noreply.github.com>
2020-07-14 20:41:33 -0700

    Add files via upload

---
+++ pages/faqPage.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+import faq from '../components/faq';
+
+
+const faqPage = () =>
+{
+
+    return(
+      <div>
+        <faq />
+        
+      </div>
+    );
+};
+
+export default faqPage;
