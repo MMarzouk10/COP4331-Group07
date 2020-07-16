266f5b1b1e1374e73f1822bed80a551676a032b4
itsokthen <65359505+itsokthen@users.noreply.github.com>
2020-07-15 21:57:32 -0400

    Create SignupPage.js

---
+++ pages/SignupPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//import SignupTitle from '../components/SignupTitle';
+import Signup from '../components/Signup';
+
+const SignupPage = () =>
+{
+
+    return(
+      <div>
+       
+        <Signup />
+      </div>
+    );
+};
+
+export default SignupPage;
