ba85e3773066d3366d66965f47d3ca315691c437
maxmc2234 <65358650+maxmc2234@users.noreply.github.com>
2020-07-15 20:18:33 -0700

    Add files via upload
    new changes after the revert

--- components/CardUI.js
+++ components/CardUI.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-
 const BASE_URL = 'https://mernstack-1.herokuapp.com/';
-//const BASE_URL = 'http://localhost:5000/';
-
 function CardUI()
 {
     var card = '';
@@ -18,7 +15,9 @@ function CardUI()
     var firstName = ud.firstName;
     var lastName = ud.lastName;
 
-    const addCard = async event => 
+
+
+    const addCard = async event =>
     {
 	    event.preventDefault();
 
@@ -48,54 +47,73 @@ function CardUI()
 
 	};
 
-  const searchCard = async event => 
-  {
-      event.preventDefault();
-        
-      var js = '{"userId":"'+userId+'","search":"'+search.value+'"}';
 
-      try
-      {
-          const response = await fetch(BASE_URL + 'api/searchcards',
-          {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+    const searchCard = async event =>
+    {
+        event.preventDefault();
 
-          var txt = await response.text();
-          var res = JSON.parse(txt);
-          var _results = res.results;
-          var resultText = '';
-          for( var i=0; i<_results.length; i++ )
-          {
-              resultText += _results[i];
-              if( i < _results.length - 1 )
-              {
-                  resultText += ', ';
-              }
-          }
-          setResults('Card(s) have been retrieved');
-          setCardList(resultText);
-      }
-      catch(e)
-      {
-          alert(e.toString());
-          setResults(e.toString());
-      }
-    }
+        var js = '{"userId":"'+userId+'","search":"'+search.value+'"}';
+
+        try
+        {
+            const response = await fetch('http://localhost:5000/api/searchcards',
+            {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+            var txt = await response.text();
+            var res = JSON.parse(txt);
+            var _results = res.results;
+            var resultText = '';
+            for( var i=0; i<_results.length; i++ )
+            {
+                resultText += _results[i];
+                if( i < _results.length - 1 )
+                {
+                    resultText += ', ';
+                }
+            }
+            setResults('Card(s) have been retrieved');
+            setCardList(resultText);
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            setResults(e.toString());
+        }
+    };
+
+    const goProfile = async event =>
+    {
+        window.location.href = '/profile';
+    };
+	const faq = async event =>
+    {
+        window.location.href = '/faq';
+    };
+
+
+
     return(
-      <div id="cardUIDiv">
+        <div id="cardUIDiv">
         <br />
-        <input type="text" id="searchText" placeholder="Card To Search For" 
+        <button type="button" id="ProfilePage" class="buttons"
+          onClick={goProfile}> Profile </button><br />
+        <br />
+        <button type="button" id="FaqPage" class="buttons"
+          onClick={Faq}> FAQ </button><br />
+        <br />
+        <input type="text" id="searchText" placeholder="Card To Search For"
           ref={(c) => search = c} />
-        <button type="button" id="searchCardButton" class="buttons" 
+        <button type="button" id="searchCardButton" class="buttons"
           onClick={searchCard}> Search Card</button><br />
         <span id="cardSearchResult">{searchResults}</span>
         <p id="cardList">{cardList}</p><br /><br />
-        <input type="text" id="cardText" placeholder="Card To Add" 
+        <input type="text" id="cardText" placeholder="Card To Add"
           ref={(c) => card = c} />
-        <button type="button" id="addCardButton" class="buttons" 
+        <button type="button" id="addCardButton" class="buttons"
           onClick={addCard}> Add Card </button><br />
         <span id="cardAddResult">{message}</span>
       </div>
-      
+
     );
 }
 
