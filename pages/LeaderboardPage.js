5b12d67ec3f005edad14271d6d5d067d33699923
itsokthen <65359505+itsokthen@users.noreply.github.com>
2020-07-16 22:06:05 -0400

    Create LeaderboardPage.js

---
+++ pages/LeaderboardPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import LeaderboardUI from '../components/LeaderboardUI';
+
+const LeaderboardPage = () =>
+{
+
+    return(
+      <div>
+        <LeaderboardUI />
+        
+      </div>
+    );
+};
+
+export default LeaderboardPage;
