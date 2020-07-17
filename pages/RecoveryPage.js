0883f7fc6bc5011a81d1c80a0853d35bf13673cf
itsokthen <65359505+itsokthen@users.noreply.github.com>
2020-07-16 23:46:01 -0400

    Create RecoveryPage.js

---
+++ pages/RecoveryPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//import SignupTitle from '../components/SignupTitle';
+import RecoveryUI from '../components/RecoveryUI';
+
+const RecoveryPage = () =>
+{
+
+    return(
+      <div>
+        <RecoveryUI />
+        
+      </div>
+    );
+};
+
+export default RecoveryPage;
