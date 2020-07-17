140517ab5ef61f2affbf89f293893038a1a35bfa
itsokthen <65359505+itsokthen@users.noreply.github.com>
2020-07-16 22:53:19 -0400

    Update LeaderboardUI.js

--- components/LeaderboardUI.js
+++ components/LeaderboardUI.js
@@ -1,74 +1,88 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-const BASE_URL = 'https://mernstack-1.herokuapp.com/';
-//FOR LOCAL TESTING USE LOCALHOST URL
-//const BASE_URL = 'http://localhost:5000/';
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: 'purple',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
-function LeaderboardUI()
-{
-    var loginName;
-    var loginPassword;
-    var emailName;
+const StyledTableRow = withStyles((theme) => ({
+  width: 300,
+     root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
 
-   // const [message,setMessage] = useState('');
+function createData(Rank, User, Points) {
+  return { Rank, User, Points };
+}
 
-    const doSignup = async event => 
+const rows = [
+  createData('1', 'Angelina Jolie', '10.0'),
+  createData('2', 'Beyonce', '9.2'),
+  createData('3', 'Rihanna', '8.7'),
+  createData('4', 'Tupac', '7.1'),
+  createData('5', 'Aaron Rodger', '7.0'),
+  createData('6', 'Daniel Biller', '6.5'),
+  createData('7', 'Jack Sparrow', '6.3'),
+  createData('8', 'Tom Brady', '6.1'),
+  createData('9', 'The Rock', '5.7'),
+  createData('10', 'Spongebob', '0.1')
+  
+];
 
-    
-    {
-        
-        event.preventDefault();
+const useStyles = makeStyles({
+  table: {
+    minWidth: 300,
+  },
+});
 
-        var js = '{"email":"'
-                + emailName.value
-                + '","login":"'
-            + loginName.value
-            + '","password":"'
-            + loginPassword.value +'"}';
+export default function PageTitle() {
+  const classes = useStyles();
 
-            try
-        {    
-            const response = await fetch(BASE_URL + 'api/signup',
-                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
-
-            var res = JSON.parse(await response.text());
+  return (
+    <div >
+    <h2 >Ultra Trivia - Leaderboard</h2>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow width= "50">
+            <StyledTableCell align="center" >Rank</StyledTableCell>
+            <StyledTableCell align="center" >User</StyledTableCell>
+            <StyledTableCell align="center" >Score</StyledTableCell>
             
-            if( res.id > 0 )
-            {
-                //setMessage('User already exists');
-                alert("user exists already");
-            }
-            else
-            {
-                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
-                localStorage.setItem('user_data', JSON.stringify(user));
-
-               // setMessage('');
-                window.location.href = '/';
-            }
-        }
-        catch(e)
-        {
-            alert(e.toString());
-            return;
-        }    
-    };
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name} width="50">
+              
+              <StyledTableCell align="center">{row.Rank}</StyledTableCell>
+              <StyledTableCell align="center">{row.User}</StyledTableCell>
+              <StyledTableCell align="center">{row.Points}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </div>
+  );
+  
+}
 
 
-    return(
-      <div id="loginDiv">
-        <form onSubmit={doSignup}>
-        <span id="inner-title">PLEASE SIGN UP</span><br />
-        <input type="text" id="emailName" placeholder="email" ref={(c) => emailName = c} /><br />
-        <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} /><br />
-        <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} /><br />
-        <input type="submit" id="signupButton" class="buttons" value = "Sign Up"
-          onClick={doSignup} />
 
-        </form>
-        
-     </div>
-    );
-};
-
-export default LeaderboardUI;
