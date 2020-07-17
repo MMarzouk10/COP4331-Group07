f89d54c37ba6626147cb2316d9338f8f8b712501
itsokthen <65359505+itsokthen@users.noreply.github.com>
2020-07-16 23:46:27 -0400

    Update ProfileUI.js

--- components/ProfileUI.js
+++ components/ProfileUI.js
@@ -20,7 +20,7 @@ function ProfileUI()
 
 
     return(
-        <div id="profileUIDiv" style={{width: '100%', height: '100%', backgroundColor:'purple', position:'absolute'}} >
+        <div id="profileUIDiv" style={{width: '100%', height: '100%', backgroundImage: "linear-gradient(to bottom, #4A148C,#673AB7, #9C27B0)", position:'absolute'}} >
         <h1 id="title"> Profile Page </h1>
         <button style={{backgroundColor:'lightblue', width: '135px'}} type="button" id="homePage" class="buttons" 
           onClick={goHome}> Home </button><br />
