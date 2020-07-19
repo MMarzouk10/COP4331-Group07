20da6935c8ab478b68ab6f60d2161448d377ec12
M7CORE <65249806+M7CORE@users.noreply.github.com>
2020-07-19 12:57:03 -0700

    Update CardUI.js

--- components/CardUI.js
+++ components/CardUI.js
@@ -1,33 +1,31 @@
 import React, { useState } from 'react';
+
 const BASE_URL = 'https://mernstack-1.herokuapp.com/';
+//const BASE_URL = 'http://localhost:5000/';
+
 function CardUI()
 {
-    var card = '';
+    var add = '';
     var search = '';
-    var user={}
+    var user = {};
 
     const [message,setMessage] = useState('');
     const [searchResults,setResults] = useState('');
-    const [cardList,setCardList] = useState('');
+    const [friendList,setFriendList] = useState('');
 
     var _ud = localStorage.getItem('user_data');
     var ud = JSON.parse(_ud);
-    var userId = ud.id;
-    var firstName = ud.firstName;
-    var lastName = ud.lastName;
+    var email = ud.email;
 
-
-
-    const addCard = async event => 
+    const addFriend = async event => 
     {
 	    event.preventDefault();
 
-        var js = '{"userId":"'+userId+'","card":"'+card.value+'"}';
+        var js = '{"email":"'+email+'","addFriend":"'+add.value+'"}';
 
         try
         {
-        	const response = await fetch(BASE_URL + 'api/addcard',
-           // const response = await fetch('http://localhost:5000/api/addcard',
+            const response = await fetch(BASE_URL + 'api/addfriend',
             {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
             var txt = await response.text();
@@ -39,7 +37,7 @@ function CardUI()
             }
             else
             {
-                setMessage('Card has been added');
+                setMessage('Friend has been added');
             }
         }
         catch(e)
@@ -49,82 +47,80 @@ function CardUI()
 
 	};
 
+  const searchFriend = async event => 
+  {
+      event.preventDefault();
+        
+      var js = '{"email":"'+email+'","search":"'+search.value+'"}';
 
-    const searchCard = async event => 
-    {
-        event.preventDefault();
-        	
-        var js = '{"userId":"'+userId+'","search":"'+search.value+'"}';
+      try
+      {
+          const response = await fetch(BASE_URL + 'api/searchfriend',
+          {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
-        try
-        {
-            //const response = await fetch('http://localhost:5000/api/searchcards',
-            const response = await fetch(BASE_URL + 'api/searchcards',
-            {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+          var txt = await response.text();
+          var res = JSON.parse(txt);
+          var _results = res.results;
+          var resultText = '';
+          for( var i=0; i<_results.length; i++ )
+          {
+              resultText += _results[i];
+              if( i < _results.length - 1 )
+              {
+                  resultText += ', ';
+              }
+          }
+          setResults('Friends(s) have been retrieved');
+          setFriendList(resultText);
+      }
+      catch(e)
+      {
+          alert(e.toString());
+          setResults(e.toString());
+      }
+  };
 
-            var txt = await response.text();
-            var res = JSON.parse(txt);
-            var _results = res.results;
-            var resultText = '';
-            for( var i=0; i<_results.length; i++ )
-            {
-                resultText += _results[i];
-                if( i < _results.length - 1 )
-                {
-                    resultText += ', ';
-                }
-            }
-            setResults('Card(s) have been retrieved');
-            setCardList(resultText);
-        }
-        catch(e)
-        {
-            alert(e.toString());
-            setResults(e.toString());
-        }
-    };
+  const doLogout = event => 
+  {
+    event.preventDefault();
+  
+    localStorage.removeItem("user_data")
+    window.location.href = '/';
+  }; 
 
-    const goProfile = async event => 
+  const goProfile = async event =>
     {
         window.location.href = '/profile';
-    };
+  };
 
-	  const faq = async event => 
+	const faq = async event =>
     {
         window.location.href = '/faq';
-    };
+  };
 
-    const doLogout = event => 
-    {
-	    event.preventDefault();
-		
-        alert('doLogout');
-    }; 
-
-    return(
-        <div id="cardUIDiv" style={{width: '100%', height: '100%', backgroundImage: "linear-gradient(to bottom, #4A148C,#673AB7, #9C27B0)", position:'absolute'}}>
+  return(
+    <div id="cardUIDiv" style={{width: '100%', height: '100%', backgroundImage: "linear-gradient(to bottom, #4A148C,#673AB7, #9C27B0)", position:'absolute'}}>
         <h1 style={{color:'white'}}>WELCOME user</h1>
-        <span id="userName">Logged In As John Doe </span><br />
+        <span id="userName">Logged In As {email} </span><br />
         <button style={{backgroundColor:'lightblue', width: '135px'}} type="button" id="logoutButton" class="buttons" 
            onClick={doLogout}> Log Out </button>
         <button style={{backgroundColor:'lightblue', width: '135px'}} type="button" id="profilePage" class="buttons" 
           onClick={goProfile}> Profile </button>
         <button style={{backgroundColor:'lightblue', width: '135px'}} type="button" id="faqPage" class="buttons" 
           onClick={faq}> FAQ </button><br />
-        <input type="text" id="searchText" placeholder="Card To Search For" 
+        <input type="text" id="searchText" placeholder="Search for a friend" 
           ref={(c) => search = c} />
-        <button style={{backgroundColor:'lightblue', width: '135px'}} type="button" id="searchCardButton" class="buttons" 
-          onClick={searchCard}> Search Card</button>
-        <span id="cardSearchResult">{searchResults}</span>
-        <p id="cardList">{cardList}</p><br />
-        <input type="text" id="cardText" placeholder="Card To Add" 
-          ref={(c) => card = c} />
-        <button style={{backgroundColor:'lightblue', width: '135px'}} type="button" id="addCardButton" class="buttons" 
-          onClick={addCard}> Add Card </button>
-        <span id="cardAddResult">{message}</span>
+        <button style={{backgroundColor:'lightblue', width: '135px'}} type="button" id="searchFriendButton" class="buttons" 
+          onClick={searchFriend}> Search Friend</button>
+        <span id="friendSearchResult">{searchResults}</span>
+        <p id="friendList">{friendList}</p><br />
+        <input type="text" id="addText" placeholder="Add a friend" 
+          ref={(c) => add = c} />
+        <button style={{backgroundColor:'lightblue', width: '135px'}} type="button" id="addFriendButton" class="buttons" 
+          onClick={addFriend}> Add Friend </button>
+        <span id="friendAddResult">{message}</span>
       </div>
-      
-    );
+  );
 }
 
 export default CardUI;
