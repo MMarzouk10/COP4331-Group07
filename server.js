edca092f865796bfa55ab2c091c9d79a3787ad18
M7CORE <65249806+M7CORE@users.noreply.github.com>
2020-07-19 13:13:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,6 @@ const cors = require('cors');
 //const nodemailer = require("nodemailer");
 //const math = require("mathjs");
 
-//SETUP FOR EMAIL VERIFICATION
-/*
-  var smtpTransport = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user:"UltraTrivia@gmail.com",
-    pass: "cop4331pass"
-  }
-});
-
-var rand, mailOptions, host, link;
-
-//var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
-//END EMAIL VERIFICATION
-*/
-
 /////////////////////////////////////////
 // Added for Heroku deployment.
 const path = require('path');
@@ -36,7 +20,6 @@ app.set('port', (process.env.PORT || 5000));
 
 const MongoClient = require('mongodb').MongoClient;
 
-//const url = 'mongodb+srv://RickLeinecker:COP4331Rocks@cluster0-4pisv.mongodb.net/COP4331?retryWrites=true&w=majority';
 // Changed for Heroku deployment.
 //const url = process.env.MONGODB_URI;
 const url = 'mongodb+srv://cardsuser:cop4331@cluster0.0mbkf.mongodb.net/COP4331?retryWrites=true&w=majority';
@@ -53,11 +36,6 @@ console.log("After connect");
 
 ///////////////////////////////////////////////////
 // For Heroku deployment
-//if( process.env.NODE_ENV == 'production')
-//{
-
-///////////////////////////////////////////////////
-// For Heroku deployment
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 console.log("After app.use()");
@@ -69,20 +47,20 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
 });
 
-app.post('/api/addcard', async (req, res, next) =>
+app.post('/api/addfriend', async (req, res, next) =>
 {
   // incoming: userId, card
   // outgoing: error
 
-  const { userId, card } = req.body;
+  const { email, addFriend } = req.body;
 
-  const newCard = {Card:card,UserId:userId};
+  const newFriend = {Email:email,Friend:addFriend};
   var error = '';
 
   try
   {
     const db = client.db();
-    const result = db.collection('cards').insertOne(newCard);
+    const result = db.collection('friends').insertOne(newFriend);
   }
   catch(e)
   {
@@ -115,34 +93,6 @@ app.post('/api/signup', async (req, res, next) =>
   {
     error = e.toString();
   }
-
-  /*
-  //EMAIL VERIFICATION
-  rand = math.floor((math.random() * 100) + 54);//Generate random token
-  link = "http://"+req.get('host')+"/verify?id="+rand;
-  console.log("Random Value"+rand);
-  const datab = client.db();
-  const update = datab.collection('users').update({Email:email}, {$set:{Token:rand}});//set token to email
-  mailOptions={
-  //  from: '"Ultra Trivia" <UltraTrivia@verification.com>,
-    to: email,
-    subject: "Please confirm your email account",
-    html: "Hello, <br> Please click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
-  };
-
-  console.log(mailOptions);
-  smtpTransport.sendMail(mailOptions, function(error, response){
-   if (error){
-      console.log(error);
-      res.end("error");
-    }
-    else{
-      console.log("Email sent: " + response.message);
-      res.end("sent");
-    }
-  });
-  //END EMAIL VERIFICATION
-  */
 
   var ret = {error:error};
   res.status(200).json(ret);
@@ -191,52 +141,27 @@ app.post('/api/login', async (req, res, next) =>
   res.status(200).json(ret);
 });
 
-app.post('/api/searchcards', async (req, res, next) => 
+app.post('/api/searchfriend', async (req, res, next) => 
 {
-  // incoming: userId, search
+  // incoming: email, search
   // outgoing: results[], error
 
   var error = '';
 
-  const { userId, search } = req.body;
+  const { email, search } = req.body;
 
   var _search = search.trim();
   
   const db = client.db();
-  const results = await db.collection('cards').find({"Card":{$regex:_search+'.*', $options:'r'}, UserId:userId}).toArray();
+  const results = await db.collection('friends').find({"Friend":{$regex:_search+'.*', $options:'r'}, "Email":email}).toArray();
   
   var _ret = [];
   for( var i=0; i<results.length; i++ )
   {
-    _ret.push( results[i].Card );
+    _ret.push( results[i].Friend);
   }
 
   var ret = {results:_ret, error:''};
-  res.status(200).json(ret);
-});
-
-app.post('/api/changelogin', async (req, res, next) =>
-{
-  // incoming: userId, newlogin
-  // outgoing: error
-
-  var error = '';
-
-  const {userId, newLogin} = req.body; //Get user and new login from body
-
-  const db = client.db(); //set client for db
-  //const result = await db.collection('users').find({UserId:userId}); //find the user
-
-  try
-  {
-    db.collection('users').update({_id: userId}, {$set: {Login:newLogin}}); //update the login field
-  }
-  catch(e)
-  {
-    error = e.toString();
-  }
-
-  var ret = {userId: userId, error: error};
   res.status(200).json(ret);
 });
 
@@ -262,31 +187,9 @@ app.post('/api/changepassword', async (req, res, next) =>
   res.status(200).json(ret);
 });
 
-app.post('/api/changemail', async (req, res, next) =>
-{
-  // incoming: userId, newEmail
-  // outgoing: error
-
-  var error = '';
-
-  const {userId, newEmail} = req.body;//Get user and new password login from body
-
-  const db = client.db();//set client db
-
-  try{
-    db.collection('users').update({_id: userId}, {$set: {Email: newEmail}}); //update the password field
-  }
-  catch(e){
-    error = e.toString();
-  }
-
-  var ret = {error: error};
-  res.status(200).json(ret);
-});
-
 app.post('/api/getPoints', async (req, res, next) => 
 {
-  // incoming: login
+  // incoming: email
   // outgoing: points, error
 
  var error = '';
@@ -314,42 +217,12 @@ app.post('/api/getQuestions', async (req, res, next) =>
 
   var error = '';
 
-  const { category, usedQuestions } = req.body;
+  const { category} = req.body;
 
   const db = client.db();
   const results = await db.collection('questions').find({Category:category}).toArray();
 
   var ret = { QuestionArray : results, error:''};
-  res.status(200).json(ret);
-});
-
-app.post('/api/answerQuestion', async (req, res, next) => 
-{
-  // incoming: question, answer
-  // outgoing: isCorrect
-
-  var error = '';
-
-  const { question, answer } = req.body;
-
-
-  const db = client.db();
-  const results = await db.collection('questions').find({Question:question}).toArray();
-
-  if( results.length > 0 )
-  {
-    correctAnswer = results[0].Answer;
-  }
-  else
-  {
-    error = "Question not found";
-    isCorrect = false;
-  }
-
-  var isCorrect = answer.equals(correctAnswer);
-
-  var ret = { Correct: isCorrect, error:''};
-
   res.status(200).json(ret);
 });
 
@@ -360,12 +233,11 @@ app.post('/api/incrementPoints', async (req, res, next) =>
 
   const {email, addVal} = req.body;
   var error = '';
-  var result;
 
   try
   {
     const db = client.db();
-    const result = db.collection('users').update({Email: userId}, {$inc: {Points: addVal}});
+    const result = db.collection('users').update({Email: email}, {$inc: {Points: addVal}});
   }
   catch(e)
   {
@@ -381,36 +253,44 @@ app.post('/api/setPoints', async (req, res, next) =>
   // incoming: email, points
   // outgoing: error
 
-  const { email, points } = req.body;
+  const { email, setVal } = req.body;
   var error = '';
-  var result = "Failed";
 
   try
   {
     const db = client.db();
-    const result = db.collection('users').update({Email:email}, {$set: {Points:points}});
+    const result = db.collection('users').update({Email:email}, {$set: {Points:setVal}});
   }
   catch(e)
   {
     error = e.toString();
   }
 
-  var ret = { Result:result, error: error };
+  var ret = {error: error };
   res.status(200).json(ret);
 });
 
-app.use((req, res, next) => 
+app.post('/api/leaderboard', async (req, res, next) => 
 {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
+  // incoming: 
+  // outgoing: results, error
+
+  var error = '';
+
+  const { numPlayers } = req.body;
+
+  const db = client.db();
+  const results = await db.collection('users').find().sort({"Points": -1}).toArray();
+//  const results = await db.collection('users').find( { $query: {}, $orderby: { Points : -1 }, $slice: 2 } );
+
+  var _ret = [];
+  for( var i=0; i<results.length; i++ )
+  {
+    _ret.push( results[i].Email + ": " + results[i].Points );
+  }
+
+  var ret = { Results: _ret, error: error};
+  res.status(200).json(ret);
 });
 //}
 
