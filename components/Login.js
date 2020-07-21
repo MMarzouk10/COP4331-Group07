46cf8646b19fe389dc4001eb50760d509113fa9d
M7CORE <65249806+M7CORE@users.noreply.github.com>
2020-07-20 19:56:37 -0700

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -23,7 +23,7 @@ export default () => {
         console.log("onSuccess:", data);
         var user = {email:email};
         localStorage.setItem('user_data', JSON.stringify(user));
-        window.location.href = '/cards';
+        window.location.href = '/home';
       },
 
       onFailure: err => {
@@ -42,19 +42,27 @@ export default () => {
               window.location.href = '/signup';
        
   };
+
+  const doForgotPassword = async event =>
+  {
+    window.location.href='/ForgotPassword';
+  };
+  
   return (
-    <div>
+    <div id="loginDiv" style={{textAlign:'center', width: '100%', height: '100%', backgroundImage: "linear-gradient(to bottom, #4A148C,#673AB7, #9C27B0)", position:'absolute', left:'50%', top:'50%', transform: 'translate(-50%, -50%)', display: 'flex', alignItems:'center',justifyContent:'center'}}>
       <form onSubmit={onSubmit}>
-        <input value={email} onChange={event => setEmail(event.target.value)} />
+      <h1 style={{color:'white', fontSize: 48}}>WELCOME TO ULTRA TRIVIA</h1>
+      <form style={{backgroundColor:'lightblue', justifyContent: 'center', alignItems: 'center', height: '250px'}}><br />
+      <span style={{color:'black', fontSize: 30}} id="inner-title"  >PLEASE LOG IN</span><br />
+        <input style={{width: '350px', height: '50px', fontSize: 24, justifyContent: 'center', alignItems: 'center'}} value={email} onChange={event => setEmail(event.target.value)} placeholder="Email" /><br />
+        <input style={{width: '350px', height: '50px', fontSize: 24, justifyContent: 'center', alignItems: 'center'}} type = 'password' value={password}onChange={event => setPassword(event.target.value)} placeholder="Password"/><br />
 
-        <input
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-        />
-
-        <button type="submit">Login</button>
-        <input type="submit" id="loginButton" class="buttons" value = "Signup?"
-          onClick={doSignup} />
+        <button style={{fontSize: 24, height: 40, width: '125px'}} type="submit">Login</button>
+        <input style={{fontSize: 24, height: 40, width: '125px'}} type="submit" id="loginButton" class="buttons" value = "Signup?"
+          onClick={doSignup} /><br />
+        <input style={{fontSize: 24, height: 40, width: '250px'}} type="submit" id="loginButton" class="buttons" value = "Forgot Password?"
+          onClick={doForgotPassword} />
+        </form>
       </form>
     </div>
   );
