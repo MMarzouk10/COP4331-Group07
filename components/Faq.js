852826dd8f9f2d1be4cf0189a08eb2224f8a6e7d
M7CORE <65249806+M7CORE@users.noreply.github.com>
2020-07-20 19:52:31 -0700

    Update Faq.js

--- components/Faq.js
+++ components/Faq.js
@@ -10,8 +10,9 @@ import Paper from '@material-ui/core/Paper';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
-    backgroundColor: 'black',
-    color: theme.palette.common.white,
+    backgroundColor: '#FF69B4',
+    color: 'theme.palette.common.white',
+    fontWeight : 'bold',
   },
   body: {
     fontSize: 14,
@@ -22,7 +23,7 @@ const StyledTableRow = withStyles((theme) => ({
   width: 700,
      root: {
     '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
+      backgroundColor: 'lightpink',
     },
   },
 }))(TableRow);
@@ -32,11 +33,10 @@ function createData(Questions, Answers) {
 }
 
 const rows = [
-  createData('How to download mobile app?', 'click on the following link:... Or go to Google Play and search for Ultra Trivia'),
-  createData('How to play game?', 'Open mobile once logging in and select a category for the trivia'),
-  createData('How does the interaction between the website and mobile app work?', 'The mobile app is the trivia section, web app is to add friends, update settings and check leaderboards'),
-  createData('How can I reset password?', 'The web app has that option at the following page:...'),
-  createData('How can I add a friend', 'Once logging in, there will be an option at the top of the home page allowing user to add friend'),
+  createData('How to download the mobile app?', 'Go to Google Play Store and search for Ultra Trivia'),
+  createData('How do you play game?', 'Create an account at mernstack-1.herokuapp.com. Verify your account, then log in on the mobile app and select the start game option.'),
+  createData('What is the difference between the website and the mobile app?', 'The mobile app is for the gameplay, the web app is for the FAQ, to create your acccount and to check the global leaderboard'),
+  createData('How can I reset password?', 'You can go to mernstack-1.herokuapp.com and click the "Forgot Password?" button. A code will be sent to your email. You will then need to enter that code and your new password.'),
 ];
 
 const useStyles = makeStyles({
@@ -45,11 +45,20 @@ const useStyles = makeStyles({
   },
 });
 
-export default function Faq() {
+const goHome = async event => 
+    {
+        window.location.href = '/home';
+    };
+
+export default function PageTitle() {
   const classes = useStyles();
 
   return (
-    <div id="FaqDiv" style={{width: '100%', height: '100%', backgroundImage: "linear-gradient(to bottom, #4A148C,#673AB7, #9C27B0)", position:'absolute'}}>
+    <div id="faqDiv" style={{textAlign:'center', width: '100%', height: '100%', backgroundImage: "linear-gradient(to bottom, #4A148C,#673AB7, #9C27B0)", position:'absolute', left:'50%', top:'50%', transform: 'translate(-50%, -50%)', display: 'flex', alignItems:'center',justifyContent:'center'}}>
+    
+    <form style={{ width:700, justifyContent: 'center', alignItems: 'center'}}>
+    <h1 style={{color:'white', fontSize: 48}}>FAQ</h1>
+     <br />
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="customized table">
         <TableHead>
@@ -70,8 +79,13 @@ export default function Faq() {
         </TableBody>
       </Table>
     </TableContainer>
+    <br />
+    <button style={{backgroundColor:'lightblue', fontSize: 24, height: 40, width: '250px', justifyContent: 'center', alignItems: 'center'}} type="button" id="faqPage" class="buttons" 
+          onClick={goHome}> Home </button><br />
+    </form>
     </div>
   );
+  
 }
 
 
