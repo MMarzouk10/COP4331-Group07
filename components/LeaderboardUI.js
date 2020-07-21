aa3ba27c9425622f62d0403361293daf8b57b1a3
itsokthen <65359505+itsokthen@users.noreply.github.com>
2020-07-21 11:35:36 -0400

    Update LeaderboardUI.js

--- components/LeaderboardUI.js
+++ components/LeaderboardUI.js
@@ -86,11 +86,17 @@ function LeaderboardUI()
       event.preventDefault();
         
       var js = '{"numPlayers":0}';
+      
+
 
       try
       {
+          var _ud = localStorage.getItem('user_data');
+          var ud = JSON.parse(_ud);
+          var userId = ud.email;
+          var js2 = '{"email": "'+userId+'"}';
           const response = await fetch(BASE_URL + 'api/getPoints',
-          {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+          {method:'POST',body:js2,headers:{'Content-Type': 'application/json'}});
 
           var txt = await response.text();
           var res = JSON.parse(txt);
@@ -210,6 +216,7 @@ function LeaderboardUI()
             onClick={faq}> FAQ </button>
         <br /><br />
         <h1 style={{color:'white', fontSize: 48}} >Ultra Trivia - Leaderboard</h1>
+        <br /><label style={{fontSize: 24, color: 'white'}}>Your High Score: {u20}</label><br /><br />
         
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="customized table">
