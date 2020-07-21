8f1389979c860ea8b5519da51b1276976ec31fe9
M7CORE <65249806+M7CORE@users.noreply.github.com>
2020-07-20 21:00:13 -0600

    Add files via upload

---
+++ pages/ForgotPasswordPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//import SignupTitle from '../components/SignupTitle';
+import ForgotPassword from '../components/ForgotPassword';
+
+const ForgotPasswordPage = () =>
+{
+
+    return(
+      <div>
+       
+        <ForgotPassword />
+      </div>
+    );
+};
+
+export default ForgotPasswordPage;
