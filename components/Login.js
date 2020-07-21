8d9b5b1c1090e0c2eed3ac21992de7e76c3540c3
itsokthen <65359505+itsokthen@users.noreply.github.com>
2020-07-21 10:39:35 -0400

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
-import UserPool from "../UserPool";
+import UserPool from './UserPool';
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [stage, setStage] = useState(1); // 1 = email stage, 2 = code stage
+
 
   const onSubmit = event => {
     event.preventDefault();
@@ -21,14 +23,15 @@ export default () => {
     user.authenticateUser(authDetails, {
       onSuccess: data => {
         console.log("onSuccess:", data);
-        var user = {email:email};
-        localStorage.setItem('user_data', JSON.stringify(user));
+       
         window.location.href = '/home';
       },
 
       onFailure: err => {
         console.error("onFailure:", err);
         //alert("Confirm email");
+        console.error("Passwords are not the same");
+          setStage(2);
       },
 
       newPasswordRequired: data => {
@@ -42,14 +45,13 @@ export default () => {
               window.location.href = '/signup';
        
   };
-
   const doForgotPassword = async event =>
   {
-    window.location.href='/ForgotPassword';
+      window.location.href='/ForgotPassword';
   };
-  
   return (
     <div id="loginDiv" style={{textAlign:'center', width: '100%', height: '100%', backgroundImage: "linear-gradient(to bottom, #4A148C,#673AB7, #9C27B0)", position:'absolute', left:'50%', top:'50%', transform: 'translate(-50%, -50%)', display: 'flex', alignItems:'center',justifyContent:'center'}}>
+      {stage === 1 && (
       <form onSubmit={onSubmit}>
       <h1 style={{color:'white', fontSize: 48}}>WELCOME TO ULTRA TRIVIA</h1>
       <form style={{backgroundColor:'lightblue', justifyContent: 'center', alignItems: 'center', height: '250px'}}><br />
@@ -60,10 +62,28 @@ export default () => {
         <button style={{fontSize: 24, height: 40, width: '125px'}} type="submit">Login</button>
         <input style={{fontSize: 24, height: 40, width: '125px'}} type="submit" id="loginButton" class="buttons" value = "Signup?"
           onClick={doSignup} /><br />
-        <input style={{fontSize: 24, height: 40, width: '250px'}} type="submit" id="loginButton" class="buttons" value = "Forgot Password?"
+        <input style={{fontSize: 24, height: 40, width: '250px'}} type="submit" id="loginButton" class="buttons" value = "ForgotPassword?"
           onClick={doForgotPassword} />
         </form>
       </form>
+      )}
+      {stage === 2 && (
+      <form onSubmit={onSubmit}>
+      <h1 style={{color:'white', fontSize: 48}}>WELCOME TO ULTRA TRIVIA</h1>
+      <form style={{backgroundColor:'lightblue', justifyContent: 'center', alignItems: 'center', height: '300px'}}><br />
+      <span style={{color:'black', fontSize: 30}} id="inner-title"  >PLEASE LOG IN</span><br />
+        <input style={{width: '350px', height: '50px', fontSize: 24, justifyContent: 'center', alignItems: 'center'}} value={email} onChange={event => setEmail(event.target.value)} placeholder="Email" /><br />
+        <input style={{width: '350px', height: '50px', fontSize: 24, justifyContent: 'center', alignItems: 'center'}} type = 'password' value={password}onChange={event => setPassword(event.target.value)} placeholder="Password"/><br />
+
+        <button style={{fontSize: 24, height: 40, width: '125px'}} type="submit">Login</button>
+        <input style={{fontSize: 24, height: 40, width: '125px'}} type="submit" id="loginButton" class="buttons" value = "Signup?"
+          onClick={doSignup} /><br />
+        <input style={{fontSize: 24, height: 40, width: '250px'}} type="submit" id="loginButton" class="buttons" value = "ForgotPassword?"
+          onClick={doForgotPassword} />
+        <br /><br /><label style={{width: '350px', height: '50px', fontSize: 24, justifyContent: 'center', alignItems: 'center'}}>Incorrect Password Combination</label>
+        </form>
+      </form>
+      )}
     </div>
   );
 };
